e1922bc0c2dd9a40659dde6157f671fa02179409
KaeSar <e.keser21@googlemail.com>
2025-02-07 00:44:02 +0100

    children reactnode

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export const metadata = {
   },
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
