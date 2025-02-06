3026cd38042b6693707427e9992619ae50decb57
KaeSar <e.keser21@googlemail.com>
2025-02-07 00:46:45 +0100

    fixedtypes

--- app/layout.jsx
+++ app/layout.jsx
@@ -34,7 +34,7 @@ export const metadata = {
   },
 };
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
