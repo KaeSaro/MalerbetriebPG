b4ad3c91765832bdc50dfd1d2ab78242667cfe44
KaeSarO <e.keser21@googlemail.com>
2025-04-16 11:09:43 +0200

    imporessum

--- app/impressum/page.js
+++ app/impressum/page.js
@@ -7,16 +7,18 @@ import Link from "next/link";
 export default function Impressum() {
   return (
     <Content>
-      <div className="flex flex-col space-y-12">
-        <div className="space-y-6">
+      <div className="flex flex-col space-y-16">
+        {/* Header Section */}
+        <div className="flex flex-col space-y-6">
           <Title withStroke>Impressum</Title>
           <RegularText textColor="text-[#889cab]" isBold>
             Transparenz ist uns wichtig.
           </RegularText>
         </div>
 
-        <div className="space-y-8">
-          <div className="space-y-8">
+        <div className="flex flex-col space-y-8">
+          {/* Angaben gemäß § 5 TMG */}
+          <div className="space-y-4">
             <Title withStroke>Angaben gemäß § 5 TMG</Title>
             <RegularText>
               Malerbetrieb Pascal Gerdes
@@ -27,20 +29,21 @@ export default function Impressum() {
             </RegularText>
           </div>
 
-          <div className="space-y-8">
+          {/* Kontakt */}
+          <div className="space-y-4">
             <Title withStroke>Kontakt</Title>
-          </div>
-          <div className="space-y-2">
             <RegularText>Telefon: +49 (0) 176 3234 5137</RegularText>
             <RegularText>E-Mail: info@malerbetrieb-gerdes.de</RegularText>
           </div>
 
-          <div className="space-y-8">
+          {/* Inhaber */}
+          <div className="space-y-4">
             <Title withStroke>Inhaber</Title>
             <RegularText>Pascal Gerdes</RegularText>
           </div>
 
-          <div className="space-y-8">
+          {/* Berufsrechtliche Angaben */}
+          <div className="space-y-4">
             <Title withStroke>Berufsrechtliche Angaben</Title>
             <RegularText>
               Berufskammer: Handwerkskammer [Ihre zuständige Handwerkskammer]
@@ -51,26 +54,57 @@ export default function Impressum() {
             </RegularText>
           </div>
 
-          <div className="space-y-8">
+          {/* Steuernummer */}
+          <div className="space-y-4">
+            <Title withStroke>Steuernummer</Title>
+            <RegularText>Steuernummer: XXXXXXXXXXX</RegularText>
+          </div>
+
+          {/* EU-Streitschlichtung */}
+          <div className="space-y-4">
             <Title withStroke>EU-Streitschlichtung</Title>
+            <RegularText>
+              Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit:
+            </RegularText>
             <Link
               href="https://ec.europa.eu/consumers/odr/"
               target="_blank"
               rel="noopener noreferrer"
-              className="inline-block rounded-lg ml-[-8px] px-2 py-0 transition-all duration-300 ease-in-out cursor-pointer hover:bg-[#889cab] hover:text-white [&>p]:!text-[16px] md:[&>p]:!text-[20px] lg:[&>p]:!text-[24px]"
+              className="inline-block text-[#889cab] hover:underline"
             >
               <RegularText>https://ec.europa.eu/consumers/odr/</RegularText>
             </Link>
           </div>
 
-          <div className="space-y-8">
+          {/* Streitbeilegung */}
+          <div className="space-y-4">
             <Title withStroke>Streitbeilegung</Title>
             <RegularText>
-              Wir sind nicht bereit oder verpflichtet, an
-              Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle
-              teilzunehmen.
+              Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer
+              Verbraucherschlichtungsstelle teilzunehmen.
             </RegularText>
           </div>
+
+          {/* Haftung für Inhalte */}
+          <div className="space-y-4">
+            <Title withStroke>Haftung für Inhalte</Title>
+            <RegularText>
+              Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den
+              allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht
+              verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu
+              forschen, die auf eine rechtswidrige Tätigkeit hinweisen.
+            </RegularText>
+            <RegularText>
+              Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen
+              Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der
+              Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden
+              Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
+            </RegularText>
+          </div>
+
+         
+
+  
         </div>
       </div>
     </Content>
